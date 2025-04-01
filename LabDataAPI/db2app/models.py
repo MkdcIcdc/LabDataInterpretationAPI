a7fac3550feea598d6d59cd19fecac718fc34c9b
Mariya Polshkova <polshkovamamariya@gmail.com>
2025-04-01 15:19:29 +0300

    medstat models created + serializers

--- LabDataAPI/db2app/models.py
+++ LabDataAPI/db2app/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
 
-class DB2Data(models.Model):
+class MedstatData(models.Model):
     
     """ Модель для хранения данных из БД """
     
-    patient_key = models.CharField(max_length=100, unique=True) #SQL KEY
     research_key = models.CharField(max_length=100, unique=True) #SQL KEY_RESEARCH
-    fullname = models.CharField(max_length=100) #SQL FIRSTNAME + MIDDLENAME + LASTNAME
     patient_result = models.JSONField() #SQL RESULTFORMZAKL
     gender = models.CharField(max_length=10) #SQL SEX
+    research_date = models.CharField(max_length=100) #SQL   RESEARCHTIME
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
--- LabDataAPI/db2app/views.py
+++ LabDataAPI/db2app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+from .models import MedstatData
+from .serializers import MedstatDataSerializer
 
-# Create your views here.
+
+class MedstatDataViewSet(viewsets.ModelViewSet):
+    
+    @action(detail=True, methods=["get"])
+    def get_patient_from_medstat(self, request, pk=None):
+        pass
+    pass
